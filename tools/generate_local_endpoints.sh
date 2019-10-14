2fb19703f1c4f69444f92eeb714dad5bb976aed8
zzboy <zzboy>
2019-10-14 18:03:26 +0800

    xxx

--- tools/generate_local_endpoints.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-curl -fsSLO --compressed "https://raw.githubusercontent.com/aliyun/aliyun-openapi-python-sdk/master/aliyun-python-sdk-core/aliyunsdkcore/data/endpoints.json"
-
-DIR=`pwd`
-
-node convert_endpoints.js > $DIR/../core/src/LocalEndpoints.h
